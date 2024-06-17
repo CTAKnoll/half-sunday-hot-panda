917cfff29a6f56a598b7916b5aef7dda49ba13da
Chris Knoll <cknoll@globalworldwide.com>
2024-06-16 23:24:11 -0700

    Moved pistol projectiles into their own folder

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -1,5 +1,4 @@
-﻿using Services;
-using Unity.VisualScripting;
+﻿using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
@@ -7,7 +6,7 @@ public abstract class Projectile : MonoBehaviour
 {
     public float Speed;
     public int Damage;
-    [DoNotSerialize] public Vector3 InitialDirection;
+    [NonSerialized] public Vector3 InitialDirection;
 
     protected abstract void Move();
 
