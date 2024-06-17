0f9bd0bc6e2b82bd3f4ec9c88d33e05105b267b8
Chris Knoll <cknoll@globalworldwide.com>
2024-06-16 23:39:32 -0700

    Bullets now look in the direction they're fired when spawned

--- Assets/Scripts/Projectiles/SimpleBullet.cs
+++ Assets/Scripts/Projectiles/SimpleBullet.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class SimpleBullet : Projectile
 {
+    private void Start()
+    {
+        transform.rotation = Quaternion.LookRotation(InitialDirection, Vector3.up);
+    }
+    
     protected override void Move()
     {
         transform.position += Speed * Time.deltaTime * InitialDirection;
